be700f5d15a7a6f6aded08cc6b059609d4ce1766
Stefan <stefanmeiwald@yahoo.de>
2014-12-10 15:58:03 +0100

    Fixed Product query

--- app/common/sphere/WrappedQueries.scala
+++ app/common/sphere/WrappedQueries.scala
@@ -6,5 +6,5 @@ import io.sphere.sdk.queries.QueryDsl
 
 trait WrappedQueries {
   def getProductQuery(page: Int, size: Int): QueryDsl[Product] =
-    new ProductQuery().withLimit(size).withOffset(page * size)
+    ProductQuery.of().withLimit(size).withOffset(page * size)
 }
