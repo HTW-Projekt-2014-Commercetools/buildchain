75f94b0dd6151e579fc06c19047b7e2988c6f7e6
Peter Gerhard <peter.gerhard90@gmail.com>
2014-12-10 14:20:23 +0100

    updated Demand to use strong types instead of primitives

--- app/model/Demand.scala
+++ app/model/Demand.scala
@@ -1,21 +1,29 @@
 package model
 
-import common.domain.{Latitude, Longitude, Location}
+import common.domain.{Latitude, Longitude, Location, Distance, Price}
 import play.api.libs.json._
 
+case class DemandId(value: String)
 case class Demand(
-	uid: String,
+	id: DemandId,
+	uid: UserId,
 	tags: String,
 	location: Location,
-	distance: Int,
-	priceMin: Double,
-	priceMax: Double)
+	distance: Distance,
+	priceMin: Price,
+	priceMax: Price)
 
 object Demand {
+
 	implicit val latitudeReads: Reads[Latitude] = (JsPath \ "lat").read[Double].map(Latitude)
-	implicit val latitudeWrites: Writes[Latitude] = Writes { (lat: Latitude) => JsNumber(lat.x) }
+	implicit val latitudeWrites: Writes[Latitude] = Writes { (lat: Latitude) => JsNumber(lat.value) }
 	implicit val longitudeReads: Reads[Longitude] = (JsPath \ "lon").read[Double].map(Longitude)
-	implicit val longitudeWrites: Writes[Longitude] = Writes { (lon: Longitude) => JsNumber(lon.x) }
+	implicit val longitudeWrites: Writes[Longitude] = Writes { (lon: Longitude) => JsNumber(lon.value) }
+
+	implicit val demandIdFormat = Json.format[DemandId]
+	implicit val userIdFormat = Json.format[UserId]
 	implicit val locationFormat = Json.format[Location]
-  implicit val demandFormat = Json.format[Demand]
+	implicit val distanceFormat = Json.format[Distance]
+	implicit val priceFormat = Json.format[Price]
+	implicit val demandFormat = Json.format[Demand]
 }
